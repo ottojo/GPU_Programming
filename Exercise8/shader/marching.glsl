1b70e660d39e40ef058dde52df1d262a18cf0ea2
Jonas Otto <jottosmail@gmail.com>
2022-06-22 21:58:25 +0200

    autoformat, box/sphere

--- Exercise8/shader/marching.glsl
+++ Exercise8/shader/marching.glsl
@@ -3,13 +3,12 @@ const float t_min = 0.0;
 const float t_max = 200.0;
 const float EPSILON = 0.0001;
 
-
 float f(vec3);
 
 /**
  * Return the shortest distance from the eyepoint to the scene surface along
- * the marching direction. If no part of the surface is found between start and end,
- * return end.
+ * the marching direction. If no part of the surface is found between start and
+ * end, return end.
  *
  * origin: the eye point, acting as the origin of the ray
  * direction: the normalized direction to march in
@@ -17,171 +16,165 @@ float f(vec3);
  * end: the max distance away from the ey to march before giving up
  */
 float raymarch(vec3 origin, vec3 direction) {
-	float start=t_min;
-	float end=t_max;
-    float depth = start;
-    for (int i = 0; i < MAX_ITERATIONS; i++) {
-        float dist = f(origin + depth * direction);
-        if (dist < EPSILON) {
-			return depth;
-        }
-        depth += dist*0.5;
-        if (depth >= end) {
-            return end;
-        }
+  float start = t_min;
+  float end = t_max;
+  float depth = start;
+  for (int i = 0; i < MAX_ITERATIONS; i++) {
+    float dist = f(origin + depth * direction);
+    if (dist < EPSILON) {
+      return depth;
     }
-    return depth;
+    depth += dist * 0.5;
+    if (depth >= end) {
+      return end;
+    }
+  }
+  return depth;
 }
 
 /**
  * Rotation matrix around the X axis.
  */
 mat3 rotateX(float theta) {
-    float c = cos(theta);
-    float s = sin(theta);
-    return mat3(
-        vec3(1, 0, 0),
-        vec3(0, c, -s),
-        vec3(0, s, c)
-    );
+  float c = cos(theta);
+  float s = sin(theta);
+  return mat3(vec3(1, 0, 0), vec3(0, c, -s), vec3(0, s, c));
 }
 
 /**
  * Rotation matrix around the Y axis.
  */
 mat3 rotateY(float theta) {
-    float c = cos(theta);
-    float s = sin(theta);
-    return mat3(
-        vec3(c, 0, s),
-        vec3(0, 1, 0),
-        vec3(-s, 0, c)
-    );
+  float c = cos(theta);
+  float s = sin(theta);
+  return mat3(vec3(c, 0, s), vec3(0, 1, 0), vec3(-s, 0, c));
 }
 
 /**
  * Rotation matrix around the Z axis.
  */
 mat3 rotateZ(float theta) {
-    float c = cos(theta);
-    float s = sin(theta);
-    return mat3(
-        vec3(c, -s, 0),
-        vec3(s, c, 0),
-        vec3(0, 0, 1)
-    );
+  float c = cos(theta);
+  float s = sin(theta);
+  return mat3(vec3(c, -s, 0), vec3(s, c, 0), vec3(0, 0, 1));
 }
 
 /**
- * Constructive solid geometry intersection operation on SDF-calculated distances.
+ * Constructive solid geometry intersection operation on SDF-calculated
+ * distances.
  */
-float intersectSDF(float distA, float distB) {
-    return max(distA, distB);
-}
+float intersectSDF(float distA, float distB) { return max(distA, distB); }
 
 /**
  * Constructive solid geometry union operation on SDF-calculated distances.
  */
-float unionSDF(float distA, float distB) {
-    return min(distA, distB);
-}
+float unionSDF(float distA, float distB) { return min(distA, distB); }
 
 /**
  * Constructive solid geometry difference operation on SDF-calculated distances.
  */
-float differenceSDF(float distA, float distB) {
-    return max(distA, -distB);
-}
+float differenceSDF(float distA, float distB) { return max(distA, -distB); }
 
 /**
  * Signed distance function for a cube centered at the origin
  * with dimensions specified by size.
  */
 float boxSDF(vec3 p, vec3 size) {
-    vec3 d = abs(p) - (size / 2.0);
-    
-    // Assuming p is inside the cube, how far is it from the surface?
-    // Result will be negative or zero.
-    float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
-    
-    // Assuming p is outside the cube, how far is it from the surface?
-    // Result will be positive or zero.
-    float outsideDistance = length(max(d, 0.0));
-    
-    return insideDistance + outsideDistance;
+  vec3 d = abs(p) - (size / 2.0);
+
+  // Assuming p is inside the cube, how far is it from the surface?
+  // Result will be negative or zero.
+  float insideDistance = min(max(d.x, max(d.y, d.z)), 0.0);
+
+  // Assuming p is outside the cube, how far is it from the surface?
+  // Result will be positive or zero.
+  float outsideDistance = length(max(d, 0.0));
+
+  return insideDistance + outsideDistance;
 }
 
 /**
  * Signed distance function for a sphere centered at the origin with radius r.
  */
-float sphereSDF(vec3 p, float r) {
-    return length(p) - r;
-}
+float sphereSDF(vec3 p, float r) { return length(p) - r; }
 
 /**
- * Signed distance function for an XY aligned cylinder centered at the origin with
- * height h and radius r.
+ * Signed distance function for an XY aligned cylinder centered at the origin
+ * with height h and radius r.
  */
 float cylinderSDF(vec3 p, float h, float r) {
-    // How far inside or outside the cylinder the point is, radially
-    float inOutRadius = length(p.xy) - r;
-    
-    // How far inside or outside the cylinder is, axially aligned with the cylinder
-    float inOutHeight = abs(p.z) - h/2.0;
-    
-    // Assuming p is inside the cylinder, how far is it from the surface?
-    // Result will be negative or zero.
-    float insideDistance = min(max(inOutRadius, inOutHeight), 0.0);
+  // How far inside or outside the cylinder the point is, radially
+  float inOutRadius = length(p.xy) - r;
 
-    // Assuming p is outside the cylinder, how far is it from the surface?
-    // Result will be positive or zero.
-    float outsideDistance = length(max(vec2(inOutRadius, inOutHeight), 0.0));
-    
-    return insideDistance+outsideDistance;
+  // How far inside or outside the cylinder is, axially aligned with the
+  // cylinder
+  float inOutHeight = abs(p.z) - h / 2.0;
+
+  // Assuming p is inside the cylinder, how far is it from the surface?
+  // Result will be negative or zero.
+  float insideDistance = min(max(inOutRadius, inOutHeight), 0.0);
+
+  // Assuming p is outside the cylinder, how far is it from the surface?
+  // Result will be positive or zero.
+  float outsideDistance = length(max(vec2(inOutRadius, inOutHeight), 0.0));
+
+  return insideDistance + outsideDistance;
 }
 
 /**
  * Signed distance function describing the scene.
- * 
+ *
  * Absolute value of the return value indicates the distance to the surface.
  * Sign indicates whether the point is inside or outside the surface,
  * negative indicating inside.
  */
-float f(vec3 samplePoint) {    
-    // Slowly spin the whole scene   
-    samplePoint = rotateY(iTime / 2.0) * samplePoint;
-    //TODO - Create your scene.
-    return 10.0;
+float f(vec3 samplePoint) {
+  // Slowly spin the whole scene
+  samplePoint = rotateY(iTime / 2.0) * samplePoint;
+  // TODO - Create your scene.
+
+  /*
+  // Circle with radius 0.5
+  return length(samplePoint) - 0.5;
+  */
+
+  /*
+  // Box:
+  vec3 sizes = vec3(0.5, 0.5, 0.5);
+  vec3 d = abs(samplePoint) - sizes;
+  return length(max(d, 0))+min(max(d.x, max(d.y,d.z)), 0);
+  */
+
+  // return 10.0;
 }
 
-
 /**
- * Return the normalized direction to march in from the eye point for a single pixel.
- * 
+ * Return the normalized direction to march in from the eye point for a single
+ * pixel.
+ *
  * fieldOfView: vertical field of view in degrees
  * size: resolution of the output image
  * fragCoord: the x,y coordinate of the pixel in the output image
  */
 vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
-    vec2 xy = fragCoord - size / 2.0;
-    float z = size.y / tan(radians(fieldOfView) / 2.0);
-    return normalize(vec3(xy, -z));
+  vec2 xy = fragCoord - size / 2.0;
+  float z = size.y / tan(radians(fieldOfView) / 2.0);
+  return normalize(vec3(xy, -z));
 }
 
 /**
  * Using the gradient of the SDF, estimate the normal on the surface at point p.
  */
 vec3 estimateNormal(vec3 p) {
-    return normalize(vec3(
-        f(vec3(p.x + EPSILON, p.y, p.z)) - f(vec3(p.x - EPSILON, p.y, p.z)),
-        f(vec3(p.x, p.y + EPSILON, p.z)) - f(vec3(p.x, p.y - EPSILON, p.z)),
-        f(vec3(p.x, p.y, p.z  + EPSILON)) - f(vec3(p.x, p.y, p.z - EPSILON))
-    ));
+  return normalize(vec3(
+      f(vec3(p.x + EPSILON, p.y, p.z)) - f(vec3(p.x - EPSILON, p.y, p.z)),
+      f(vec3(p.x, p.y + EPSILON, p.z)) - f(vec3(p.x, p.y - EPSILON, p.z)),
+      f(vec3(p.x, p.y, p.z + EPSILON)) - f(vec3(p.x, p.y, p.z - EPSILON))));
 }
 
 /**
  * Lighting contribution of a single point light source via Phong illumination.
- * 
+ *
  * The vec3 returned is the RGB color of the light's contribution.
  *
  * k_a: Ambient color
@@ -197,30 +190,30 @@ vec3 estimateNormal(vec3 p) {
  */
 vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
                           vec3 lightPos, vec3 lightIntensity) {
-    vec3 N = estimateNormal(p);
-    vec3 L = normalize(lightPos - p);
-    vec3 V = normalize(eye - p);
-    vec3 R = normalize(reflect(-L, N));
-    
-    float dotLN = dot(L, N);
-    float dotRV = dot(R, V);
-    
-    if (dotLN < 0.0) {
-        // Light not visible from this point on the surface
-        return vec3(0.0, 0.0, 0.0);
-    } 
-    
-    if (dotRV < 0.0) {
-        // Light reflection in opposite direction as viewer, apply only diffuse
-        // component
-        return lightIntensity * (k_d * dotLN);
-    }
-    return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+  vec3 N = estimateNormal(p);
+  vec3 L = normalize(lightPos - p);
+  vec3 V = normalize(eye - p);
+  vec3 R = normalize(reflect(-L, N));
+
+  float dotLN = dot(L, N);
+  float dotRV = dot(R, V);
+
+  if (dotLN < 0.0) {
+    // Light not visible from this point on the surface
+    return vec3(0.0, 0.0, 0.0);
+  }
+
+  if (dotRV < 0.0) {
+    // Light reflection in opposite direction as viewer, apply only diffuse
+    // component
+    return lightIntensity * (k_d * dotLN);
+  }
+  return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
 }
 
 /**
  * Lighting via Phong illumination.
- * 
+ *
  * The vec3 returned is the RGB color of that point after lighting is applied.
  * k_a: Ambient color
  * k_d: Diffuse color
@@ -231,73 +224,68 @@ vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
  *
  * See https://en.wikipedia.org/wiki/Phong_reflection_model#Description
  */
-vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
-    const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
-    vec3 color = ambientLight * k_a;
-    
-    vec3 light1Pos = vec3(4.0 * sin(iTime),
-                          2.0,
-                          4.0 * cos(iTime));
-    vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
-    
-    color += phongContribForLight(k_d, k_s, alpha, p, eye,
-                                  light1Pos,
-                                  light1Intensity);
-    
-    vec3 light2Pos = vec3(2.0 * sin(0.37 * iTime),
-                          2.0 * cos(0.37 * iTime),
-                          2.0);
-    vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
-    
-    color += phongContribForLight(k_d, k_s, alpha, p, eye,
-                                  light2Pos,
-                                  light2Intensity);    
-    return color;
+vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p,
+                       vec3 eye) {
+  const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
+  vec3 color = ambientLight * k_a;
+
+  vec3 light1Pos = vec3(4.0 * sin(iTime), 2.0, 4.0 * cos(iTime));
+  vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
+
+  color +=
+      phongContribForLight(k_d, k_s, alpha, p, eye, light1Pos, light1Intensity);
+
+  vec3 light2Pos = vec3(2.0 * sin(0.37 * iTime), 2.0 * cos(0.37 * iTime), 2.0);
+  vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
+
+  color +=
+      phongContribForLight(k_d, k_s, alpha, p, eye, light2Pos, light2Intensity);
+  return color;
 }
 
 /**
  * Return a transform matrix that will transform a ray from view space
- * to world coordinates, given the eye point, the camera target, and an up vector.
+ * to world coordinates, given the eye point, the camera target, and an up
+ * vector.
  *
- * This assumes that the center of the camera is aligned with the negative z axis in
- * view space when calculating the ray marching direction. See rayDirection.
+ * This assumes that the center of the camera is aligned with the negative z
+ * axis in view space when calculating the ray marching direction. See
+ * rayDirection.
  */
 mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
-    // Based on gluLookAt man page
-    vec3 f = normalize(center - eye);
-    vec3 s = normalize(cross(f, up));
-    vec3 u = normalize(cross(s, f));
-    return mat3(s, u, -f);
+  // Based on gluLookAt man page
+  vec3 f = normalize(center - eye);
+  vec3 s = normalize(cross(f, up));
+  vec3 u = normalize(cross(s, f));
+  return mat3(s, u, -f);
 }
 
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+  vec3 viewDir = rayDirection(45.0, iResolution.xy, fragCoord);
+  vec3 eye = vec3(8.0, 5.0, 7.0);
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec3 viewDir = rayDirection(45.0, iResolution.xy, fragCoord);
-    vec3 eye = vec3(8.0, 5.0, 7.0);
-    
-    mat3 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
-    
-    vec3 worldDir = normalize(viewToWorld * viewDir);
-    
-    float dist = raymarch(eye, worldDir);
-    
-    if (dist > t_max - EPSILON) {
-        // Didn't hit anything
-        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
-		return;
-    }
-    
-    // The closest point on the surface to the eyepoint along the view ray
-    vec3 p = eye + dist * worldDir;
-    
-    // Use the surface normal as the ambient color of the material
-    vec3 K_a = (estimateNormal(p) + vec3(1.0)) / 2.0;
-    vec3 K_d = K_a;
-    vec3 K_s = vec3(1.0, 1.0, 1.0);
-    float shininess = 10.0;
-    
-    vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
-    
-    fragColor = vec4(color, 1.0);
+  mat3 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+
+  vec3 worldDir = normalize(viewToWorld * viewDir);
+
+  float dist = raymarch(eye, worldDir);
+
+  if (dist > t_max - EPSILON) {
+    // Didn't hit anything
+    fragColor = vec4(0.0, 0.0, 0.0, 0.0);
+    return;
+  }
+
+  // The closest point on the surface to the eyepoint along the view ray
+  vec3 p = eye + dist * worldDir;
+
+  // Use the surface normal as the ambient color of the material
+  vec3 K_a = (estimateNormal(p) + vec3(1.0)) / 2.0;
+  vec3 K_d = K_a;
+  vec3 K_s = vec3(1.0, 1.0, 1.0);
+  float shininess = 10.0;
+
+  vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
+
+  fragColor = vec4(color, 1.0);
 }
